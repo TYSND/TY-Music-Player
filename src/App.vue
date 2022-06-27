f6c7cf3d916752a8cc024116d4d037042170f758
TYSND <32159729+TYSND@users.noreply.github.com>
2022-06-27 09:06:00 +0800

    。

--- src/App.vue
+++ src/App.vue
@@ -18,6 +18,13 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /*margin-top: 60px;*/
+}
+body {
+  margin: 0;
+}
+* {
+  box-sizing: border-box;
+  font-family: Microsoft YaHei, Arial, Georgia, Serif, sans-serif;
 }
 </style>
--- src/view/PlayMusic.vue
+++ src/view/PlayMusic.vue
@@ -1,17 +1,37 @@
 <template>
   <div class="play-music">
-    123
+<!--    <audio @canplay="getDuration" @timeupdate="updateTime" ref="au" controls src="music/again.mp3">-->
+<!--      不支持-->
+<!--    </audio>-->
+
   </div>
 </template>
 
 <script>
 export default {
-  name: "PlayMusic"
+  name: "PlayMusic",
+  data () {
+    return {
+      musicDuration: 0,
+      currentTime: 0
+    }
+  },
+  methods: {
+    getDuration() {
+      this.musicDuration = this.$refs.au.duration
+      console.log('总时间', this.musicDuration); //此时可以获取到duration
+    },
+    updateTime(e) {
+      this.currentTime = e.target.currentTime;  //获取audio当前播放时间
+      console.log('当前时间', this.currentTime, parseInt(this.currentTime))
+    },
+  }
 }
 </script>
 
 <style lang="less" scoped>
 .play-music {
-
+  width: 100%;
+  height: 100vh;
 }
 </style>
